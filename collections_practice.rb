a68a7b47104cea4283e1de01c2955d76a5ac5fa2
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-14 21:11:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,11 +1,13 @@
-def sort_array_asc(integer=[])
+def sort_array_asc
   integer.sort do |a,b|
     a <=> b 
   end 
 end 
 
-def sort_array_desc(number=[])
+def sort_array_desc
   number.sort.reverse do |a,b|
     a<=>b 
   end 
 end 
+
+def sort_array_char_count
